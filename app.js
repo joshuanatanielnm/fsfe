cc0c5d71f33eb6391860a080bc99db10b9a20fe2
joshuanatanielnm <clsjoshua@gmail.com>
2021-09-19 10:56:27 +0000

    update app.js

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
 	res.send('hello world');
 });
 
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', '4file');
+	res.status(418);
+	res.send('I prefer coffe');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
